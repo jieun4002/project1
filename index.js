e2a80317b0620050cae5a280579dbab34839f15d
jieun4002 <wldms9418@gmail.com>
2021-04-15 16:09:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 $(function(){
   const swiper = new Swiper('.swiper-container', {
-    autoplay: {//자동 슬라이드
+    autoplay: {
       delay: 2000
     },
-    slidesPerView: 1,//화면당 이미지
-    spaceBetween: 30,//이미지 사이
-    loop: true//반복(첫화면으로 자연스럽게 이동) false는 다시 처음으로 되돌아가서 순차적으로 이동
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true
   });
   new Swiper('.swiper-container1', {
-    autoplay: {//자동 슬라이드
+    autoplay: {
       delay: 2000
     },
-    slidesPerView: 1,//화면당 이미지
-    spaceBetween: 5,//이미지 사이
-    loop: true//반복(첫화면으로 자연스럽게 이동) false는 다시 처음으로 되돌아가서 순차적으로 이동
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: true
   });
 
 
@@ -21,6 +21,11 @@ $(function(){
   $('#toggle_menu').on('click',function(){
     $('.nav_menu').toggleClass('active')
   });
+  
 
+  //a 속성 제거
+  $('a[href="#"]').on('click',function(e){
+    e.preventDefault();
+  })
 
 });
